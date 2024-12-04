ccce20586fa6ba6d669c1b4477784e6a2cf3410a
Thales Moraes <thales.moraes04@outlook.com>
2024-12-04 18:04:54 -0300

    add queue001

---
+++ datastructures/queue001.py
@@ -0,0 +1,18 @@
+fila = []
+print(fila) # Saída: []
+fila.append('1')
+fila.append('2')
+fila.append('3')
+fila.append('4')
+fila.append('5')
+print(fila)
+fila.pop(0)
+print(fila)
+fila.pop(0)
+print(fila)
+fila.pop(0)
+print(fila)
+fila.pop(0)
+print(fila)
+fila.pop(0)
+print(fila)
