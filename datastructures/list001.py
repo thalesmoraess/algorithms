cd2db3e3960af222ecca42bced89b0093e4a7ecb
Thales Moraes <thales.moraes04@outlook.com>
2024-12-04 17:28:20 -0300

    add list001

---
+++ datastructures/list001.py
@@ -0,0 +1,10 @@
+lista = [ 'A', 'E', 'I', 'O', 'U' ]
+print( lista[0] ) # Saída: A
+print( lista[1] ) # Saída: E
+print( lista[2] ) # Saída: I
+print( lista[-2] ) # Saída: O
+print( lista[-1] ) # Saída: U
+print( lista[-5:-2] ) # Saída: A, E, I
+print( lista[2:5] ) # Saída: I, O, U
+print( lista[:2] ) # Saída: A, E, I
+print( lista[2:] ) # Saída: I, O, U
