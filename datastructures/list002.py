2f2ddd0009888afea13ad5f9dfd8465a635540c5
Thales Moraes <thales.moraes04@outlook.com>
2024-12-04 17:46:11 -0300

    add list002 and stack001

---
+++ datastructures/list002.py
@@ -0,0 +1,14 @@
+lista = ['A', 'E', 'I', 'O', 'U']
+print(lista)
+lista.append('B')
+print(lista)
+lista.pop()
+print(lista)
+lista.pop(2)
+print(lista)
+lista.insert(2, 'I')
+print(lista)
+del lista[1]
+print(lista)
+lista.remove('A')
+print(lista)
