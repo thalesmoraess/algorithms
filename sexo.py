aef4a4ab218a93e15266cfb2999f3748fb262ca5
Thales Moraes <thales.moraes04@outlook.com>
2024-12-29 17:26:50 -0300

    Create sexo.py

---
+++ sexo.py
@@ -0,0 +1,9 @@
+sexo = input("Informe seu sexo, M ou F: ")
+
+if sexo == "M":
+    print("Sexo masculino")
+else:
+    if sexo == "F":
+        print("Sexo feminino") 
+    else:
+        print("Sexo inválido!")
