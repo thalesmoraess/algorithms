cfcb2bf7479e22d276b494e4822dbcb32f8d608f
Thales Moraes <thales.moraes04@outlook.com>
2024-12-29 17:08:23 -0300

    Create maioroumenor.py

---
+++ maioroumenor.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1 > num2:
+    print("O primeiro número é maior")
+else:
+    if num1 == num2:
+        print("Os números são iguais")
+    else:
+        print("O segundo número é maior")
