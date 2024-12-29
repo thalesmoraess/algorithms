e3dbde26546947a647c74fd73927866c5a4cca65
Thales Moraes <thales.moraes04@outlook.com>
2024-12-29 17:22:11 -0300

    Create positivoounegativo.py

---
+++ positivoounegativo.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um número: "))
+
+if num > 0:
+    print("O número é positivo")
+else:
+    if num == 0:
+        print("Nem positivo nem negativo, é 0")
+    else:
+        ("O número é negativo")
